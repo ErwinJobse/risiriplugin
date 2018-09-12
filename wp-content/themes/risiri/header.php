78a8d146d35a0d9121d201ffd7b9fb657b9c5d5a
Mextro <mdenouden@student.scalda.nl>
2018-09-12 16:00:31 +0200

    Table Start

--- wp-content/themes/risiri/header.php
+++ wp-content/themes/risiri/header.php
@@ -8,12 +8,15 @@
         <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>style.css">
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/css/jasny-bootstrap.min.css">
         <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="https://use.fontawesome.com/46636d4f0f.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js"></script>
+
+
 
     </head>
 
--- wp-content/themes/risiri/index.php
+++ wp-content/themes/risiri/index.php
@@ -9,7 +9,7 @@
 <?php get_header(); ?>
 
 <div class="table-container">
-<table class="artikel-table">
+<table class="artikel-table" cellspacing="0">
     <tr>
         <th>Artikelnummer</th>
         <th>Artikelnaam</th>
@@ -18,20 +18,37 @@
         <th>Actie</th>
     </tr>
     <tr>
-        <td><input></td>
-        <td><input></td>
-        <td><input></td>
-        <td><input></td>
-        <td>button</td>
+        <td><input type="text"></td>
+        <td><input type="text"></td>
+        <td><span id="date">.</span></td>
+        <td><input type="text"></td>
+        <td><i class="fa fa-plus plus"></i></td>
     </tr>
     <tr>
         <td>001</td>
         <td>Monitor</td>
         <td>11-09-2018</td>
         <td>Monitor van het merk Dell, 22"</td>
-        <td>buttons</td>
+        <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
     </tr>
 </table>
 </div>
+
+    <script language="JavaScript">
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        }
+        if(mm<10){
+            mm='0'+mm;
+        }
+        var today = dd+'-'+mm+'-'+yyyy;
+        document.getElementById("date").innerHTML = today;
+
+    </script>
 
 <?php get_footer(); ?>
