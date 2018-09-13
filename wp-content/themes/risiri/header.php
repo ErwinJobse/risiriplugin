e25706603e78649a49d079b02ec75aa9c873c1ef
Mextro <mdenouden@student.scalda.nl>
2018-09-13 13:33:43 +0200

    Table done

--- wp-content/themes/risiri/header.php
+++ wp-content/themes/risiri/header.php
@@ -17,6 +17,9 @@
         <script src="//cdnjs.cloudflare.com/ajax/libs/jasny-bootstrap/3.1.3/js/jasny-bootstrap.min.js"></script>
 
 
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
     </head>
 
--- wp-content/themes/risiri/index.php
+++ wp-content/themes/risiri/index.php
@@ -8,32 +8,6 @@
 
 <?php get_header(); ?>
 
-<div class="table-container">
-<table class="artikel-table" cellspacing="0">
-    <tr>
-        <th>Artikelnummer</th>
-        <th>Artikelnaam</th>
-        <th>Aanmaakdatum</th>
-        <th>Omschrijving</th>
-        <th>Actie</th>
-    </tr>
-    <tr>
-        <td><input type="text"></td>
-        <td><input type="text"></td>
-        <td><span id="date">.</span></td>
-        <td><input type="text"></td>
-        <td><i class="fa fa-plus plus"></i></td>
-    </tr>
-    <tr>
-        <td>001</td>
-        <td>Monitor</td>
-        <td>11-09-2018</td>
-        <td>Monitor van het merk Dell, 22"</td>
-        <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
-    </tr>
-</table>
-</div>
-
     <script language="JavaScript">
         var today = new Date();
         var dd = today.getDate();
@@ -50,5 +24,105 @@
         document.getElementById("date").innerHTML = today;
 
     </script>
+    <script>
+        $( function() {
+            $( "#tabs" ).tabs();
+        } );
+    </script>
+
+
+<div id="tabs">
+
+    <ul id="table-nav">
+        <li><a href="#tab-artikelen">Artikelen</a></li>
+        <li><a href="#tab-klanten">Klanten</a></li>
+    </ul>
+
+    <div id="tab-artikelen">
+        <table id="data-table" cellspacing="0">
+            <tr>
+                <th width="14%">Artikelnummer</th>
+                <th width="15%">Artikelnaam</th>
+                <th width="15%">Aanmaakdatum</th>
+                <th width="44%">Omschrijving</th>
+                <th width="5%">Actie</th>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+                <td><input type="text"></td>
+                <td><span id="date">.</span></td>
+                <td><input type="text"></td>
+                <td><i class="fa fa-plus plus"></i></td>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Monitor</td>
+                <td>11-09-2018</td>
+                <td style="text-align: left; padding-left: 15px;">Monitor van het merk Dell, 22"</td>
+                <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Monitor</td>
+                <td>11-09-2018</td>
+                <td style="text-align: left; padding-left: 15px;">Monitor van het merk Dell, 22"</td>
+                <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Monitor</td>
+                <td>11-09-2018</td>
+                <td style="text-align: left; padding-left: 15px;">Monitor van het merk Dell, 22"</td>
+                <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
+            </tr>
+        </table>
+    </div>
+    <div id="tab-klanten">
+        <table id="data-table" cellspacing="0">
+            <tr>
+                <th width="10%">Klantnummer</th>
+                <th width="10%">Voornaam</th>
+                <th width="10%">Tussenvoegsel</th>
+                <th width="10%">Achternaam</th>
+                <th width="50%">Email</th>
+                <th width="5%">Actie</th>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+                <td><input type="text"></td>
+                <td><input type="text"></td>
+                <td><input type="text"></td>
+                <td><input type="text"></td>
+                <td><i class="fa fa-plus plus"></i></td>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Max</td>
+                <td>den</td>
+                <td>Ouden</td>
+                <td style="text-align: left; padding-left: 15px;">mdenouden@student.scalda.nl</td>
+                <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Max</td>
+                <td>den</td>
+                <td>Ouden</td>
+                <td style="text-align: left; padding-left: 15px;">mdenouden@student.scalda.nl</td>
+                <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>Max</td>
+                <td>den</td>
+                <td>Ouden</td>
+                <td style="text-align: left; padding-left: 15px;">mdenouden@student.scalda.nl</td>
+                <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+
 
 <?php get_footer(); ?>
