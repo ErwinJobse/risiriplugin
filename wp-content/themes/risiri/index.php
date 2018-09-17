1f4e90ae8736bcf3f9709632d54858f8ab4927f8
Erwin Jobse <erwin@jobse.org>
2018-09-17 13:13:24 +0200

    Updated

--- wp-content/themes/risiri/index.php
+++ wp-content/themes/risiri/index.php
@@ -6,6 +6,8 @@
  */
 global $wpdb;
 
+
+
 $tableKlant = 'risiri_klanten';
 $tableArtikel = 'risiri_artikelen';
 
@@ -48,11 +50,11 @@ $getArtikel = $wpdb->get_results( "SELECT * FROM $tableArtikel" );
     <div id="tab-artikelen">
         <table id="data-table" cellspacing="0">
             <tr>
-                <th width="14%">Artikelnummer</th>
+                <th width="12%">Artikelnummer</th>
                 <th width="15%">Artikelnaam</th>
                 <th width="15%">Aanmaakdatum</th>
                 <th width="44%">Omschrijving</th>
-                <th width="5%">Actie</th>
+                <th width="7%">Actie</th>
             </tr>
 
             <tr>
@@ -62,17 +64,17 @@ $getArtikel = $wpdb->get_results( "SELECT * FROM $tableArtikel" );
                 <td><input type="text" name="Artikelnaam"></td>
                 <td><input type="text" name="Aanmaakdatum"></td>
                 <td><input type="text" name="omschrijving"></td>
-                <td><input class="fa fa-plus plus" type="submit" name="submit" value="Submit"></input></td>
+                <td><input class="fa fa-plus plus" type="submit" name="submitArtikel" value="Submit"></input></td>
                 </form>
             </tr>
             <?php
             foreach ($getArtikel as $row){ ?>
                 <tr>
                     <td><?php echo $row->Artikelnummer;?></td>
-                    <td><?php echo $row->Artikelnaam;?></td>
+                    <td onclick="listenForDoubleClick(this);"><?php echo $row->Artikelnaam;?></td>
                     <td><?php echo $row->Aanmaakdatum;?></td>
-                    <td><?php echo $row->omschrijving;?></td>
-                    <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
+                    <td onclick="listenForDoubleClick(this);"><?php echo $row->omschrijving;?></td>
+                    <td><div class="action-buttons"><a class="fas fa-pen pen" name="insert" value="insert"></a><a class="fas fa-trash-alt trash"  href="index.php?delArtikel=<?php echo $row->Artikelnummer;?>" name="delete" ></a></div></td>
 
                 </tr>
             <?php }
@@ -84,30 +86,33 @@ $getArtikel = $wpdb->get_results( "SELECT * FROM $tableArtikel" );
     <div id="tab-klanten">
         <table id="data-table" cellspacing="0">
             <tr>
-                <th width="10%">Klantnummer</th>
+                <th width="8%">Klantnummer</th>
                 <th width="10%">Voornaam</th>
                 <th width="10%">Tussenvoegsel</th>
                 <th width="10%">Achternaam</th>
                 <th width="50%">Email</th>
-                <th width="5%">Actie</th>
+                <th width="7%">Actie</th>
             </tr>
             <tr>
-                <td><input type="text"></td>
-                <td><input type="text"></td>
-                <td><input type="text"></td>
-                <td><input type="text"></td>
-                <td><input type="text"></td>
-                <td><i class="fa fa-plus plus" ></i></td>
+                <form method="post">
+
+                    <td><input type="text" name="klantnummer"></td>
+                    <td><input type="text" name="voorNaam"></td>
+                    <td><input type="text" name="TussenVoegsel"></td>
+                    <td><input type="text" name="Achternaam"></td>
+                    <td><input type="text" name="email"></td>
+                    <td><input class="fa fa-plus plus" type="submit" name="submitKlant" value="Submit"></input></td>
+                </form>
             </tr>
             <?php
             foreach ($getKlant as $row){ ?>
             <tr>
                 <td><?php echo $row->klantnummer;?></td>
-                <td><?php echo $row->voorNaam;?></td>
-                <td><?php echo $row->TussenVoegsel;?></td>
-                <td><?php echo $row->Achternaam;?></td>
-                <td><?php echo $row->email;?></td>
-                <td><div class="action-buttons"><i class="fas fa-pen pen"><i class="fas fa-trash-alt trash"></div></td>
+                <td onclick="listenForDoubleClick(this);"><?php echo $row->voorNaam;?></td>
+                <td onclick="listenForDoubleClick(this);"><?php echo $row->TussenVoegsel;?></td>
+                <td onclick="listenForDoubleClick(this);"><?php echo $row->Achternaam;?></td>
+                <td onclick="listenForDoubleClick(this);"><?php echo $row->email;?></td>
+                <td><div class="action-buttons"><a class="fas fa-pen pen" name="insert" value="insert"></a><a class="fas fa-trash-alt trash"  href="index.php?delKlant=<?php echo $row->klantnummer;?>" name="delete" ></a></div></td>
 
             </tr>
             <?php }
@@ -120,10 +125,24 @@ $getArtikel = $wpdb->get_results( "SELECT * FROM $tableArtikel" );
         </table>
     </div>
 </div>
+    <script>
+        function listenForDoubleClick(element) {
+            element.contentEditable = true;
+            setTimeout(function() {
+                if (document.activeElement !== element) {
+                    element.contentEditable = false;
+                }
+            }, 300);
+        }
+    </script>
+
+
+
 
 <?php
 
-if ( isset( $_POST['submit'] ) ) {
+//add artikel
+if ( isset( $_POST['submitArtikel'] ) ) {
 
 
 
@@ -131,7 +150,7 @@ if ( isset( $_POST['submit'] ) ) {
         $wpdb->insert($tableArtikel, array(
 
             'Artikelnaam' => $_POST['Artikelnaam'],
-            'omschrijving' => $_POST['omschrijving'],
+            'omschrijving' => $_POST['omschrijving']
 
         ),
             array('%s', '%s')
@@ -139,9 +158,62 @@ if ( isset( $_POST['submit'] ) ) {
 
 
     }
+    echo "<meta http-equiv='refresh' content='0'>";
 
 
 }
+
+//add klant
+else if ( isset( $_POST['submitKlant'] ) ) {
+
+
+
+    if (!empty($_POST['voorNaam'])) {
+        $wpdb->insert($tableKlant, array(
+
+            'voorNaam' => $_POST['voorNaam'],
+            'TussenVoegsel' => $_POST['TussenVoegsel'],
+            'Achternaam' => $_POST['Achternaam'],
+            'email' => $_POST['email']
+
+        ),
+                array('%s', '%s', '%s', '%s')
+
+
+        );
+        echo "<meta http-equiv='refresh' content='0'>";
+
+
+
+    }
+
+}
+//delete Artikel
+if (isset($_GET['delArtikel'])) {
+    $del = $_GET['delArtikel'];
+    //SQL query for deletion.
+    $wpdb->delete( $tableArtikel, array( 'Artikelnummer' => $del ) );
+
+}
+
+//delete klant
+if (isset($_GET['delKlant'])) {
+    $del = $_GET['delKlant'];
+    //SQL query for deletion.
+    $wpdb->delete( $tableKlant, array( 'Klantnummer' => $del ) );
+
+}
+
+//EDIT Artikel
+if (isset($_GET['editArtikel'])) {
+    $del = $_GET['editArtikel'];
+    //SQL query for deletion.
+    $wpdb->delete( $tableArtikel, array( 'Artikelnummer' => $del ) );
+
+}
+
+
+
 
     ?>
 
